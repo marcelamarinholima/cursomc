c4ce0c652bbc05c7b1b00bbedc60d4a2b60d8a4b
marcelamarinholima <marcelamlima@gmail.com>
2021-01-27 22:07:15 -0300

    Criando Repository e service para categoria

--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -16,7 +16,7 @@ public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
+	private Integer id; 
 	private String nome;
 	
 	public Categoria() {
